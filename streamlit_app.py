55566824bc9a8b6dbcc8f14c49a510386064f1a5
yvesmango <yvesmango@gmail.com>
2024-02-06 01:45:29 -0600

    geit

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,17 +9,45 @@ st.set_page_config(page_title="Streamlit: Skylite Travel Tracker", layout="wide"
     
 
 
-# Create API client credentials.
-credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+# # Create API client credentials.
+# credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 
+
+# @st.cache_data(ttl=600)
+# # Perform query and read data into DataFrame using pandas.read_gbq().
+# def run_query(query):
+#     df = pd.read_gbq(query, credentials=credentials, project_id="data-sciencey-things")
+#     return df
+
+# # Example query
+# query = "SELECT * FROM `data-sciencey-things.skylite_travel.flights`"
+
+# # Call the function to run the query and get the DataFrame
+# df = run_query(query)
+
+# print(df.shape)
+
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
-# Perform query and read data into DataFrame using pandas.read_gbq().
 def run_query(query):
-    df = pd.read_gbq(query, credentials=credentials, project_id="data-sciencey-things")
-    return df
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-# Example query
-query = "SELECT * FROM `data-sciencey-things.skylite_travel.flights`"
+rows = run_query("SELECT * FROM `data-sciencey-things.skylite_travel.flights LIMIT 10`")
 
-# Call the function to run the query and get the DataFrame
-df = run_query(query)
+
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row['airline'])
